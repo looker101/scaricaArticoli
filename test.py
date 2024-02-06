183a35a50df1c0165c1cc7985188a3e419f282d5
Looker <129039795+looker101@users.noreply.github.com>
2024-02-06 15:24:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,40 @@
 import openpyxl
 
-path = #file orders_status2024
+path = "C:\\Users\\miche\\Desktop\\.py\\ScaricaArticoli\\Order Status_2024.xlsx"
 
-wb = openpyxl.load_workbook(path)
-ws = wb.active
+# Apri il file Excel
+workbook = openpyxl.load_workbook(path)
 
-# crea l'input
-ricerca_ordini = str(input("Inserisci i numeri ordini che spedirai oggi separati da una virgola: "))
-numero_ordini_da_cercare = [n_ordine.strip() for n_ordine in ricerca_ordini.split(",")]
+foglio_origine = workbook.active
 
-# creazione del file dove andranno le righe relatiuve agli ordini inseriti dall'utente
-foglio_destinazione = wb.create_sheet("Ordini da Spedire Odierni")
-#foglio_destinazione.append(ws["1"]) # copio l'intestazione (1a riga)
+for riga in foglio_origine.iter_rows(min_col = 1):
+    for cella in riga:
+        if cella.value is not None:
+            cella.value = str(cella.value).strip()
 
-for numero_ordine in numero_ordini_da_cercare:
+# Richiedi all'utente di inserire i numeri di ordine (separati da virgola)
+input_numeri_ordine = input("Inserisci i numeri di ordine separati da virgola: ")
+numeri_ordine_da_cercare = [ordine.strip() for ordine in input_numeri_ordine.split(',')]
+
+# Crea il nuovo foglio di lavoro
+foglio_destinazione = workbook.create_sheet(title='Risultato')
+intestazione = [valore for valore in list(foglio_origine.iter_rows(min_row=1, max_row=1, values_only=True))[0]]
+foglio_destinazione.append(intestazione)
+
+# Cerca ciascun numero di ordine nel foglio di lavoro
+for numero_ordine in numeri_ordine_da_cercare:
     trovato = False
-    for riga in ws.iter_rows(min_row = 2, values_only = True):
+    for riga in foglio_origine.iter_rows(min_row=2, values_only=True):
         if riga[0] == numero_ordine:
             trovato = True
             foglio_destinazione.append(riga)
+            print("Operazione completata. Le righe corrispondenti sono state salvate nel nuovo foglio.")
             break
-    if not trovato:
-        print(f"Il numero di ordine {ricerca_ordini} non è stato trovato nel file")
 
-wb.save("orders_to_ship.xlsx")
-print("File salvato correttamente")
+    if riga[0] != numero_ordine:
+        print(f"Ordine {numero_ordine} non trovato nel foglio di lavoro.")
+        
+foglio_destinazione["H1"] = "Barcode"
+
+# Salva le modifiche nel file Excel
+workbook.save('tuo_file_modificato.xlsx')
